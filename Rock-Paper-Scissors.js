0d7e6be65ea75735bf896bef91fefb66bb8d5834
Christian Feliks <christian.feliks@student.tuke.sk>
2023-03-27 21:10:33 +0200

    add score and announce winner

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -1,128 +1,134 @@
-function getComputerChoice(){
-   let options = ["rock","paper","scissors"];
-   let pc_choice= Math.floor(Math.random() * 3 );
-   return options[pc_choice];
+function getComputerChoice() {
+	let options = ["rock", "paper", "scissors"];
+	let pc_choice = Math.floor(Math.random() * 3);
+	return options[pc_choice];
+}
+function disable_buttons() {
+	button_rock.disabled = true;
+	button_paper.disabled = true;
+	button_scissors.disabled = true;
 }
 
 function playRound(playerSelection, computerSelection) {
-   playerSelection = playerSelection.toLowerCase();
+	playerSelection = playerSelection.toLowerCase();
 
-   if (playerSelection === computerSelection) {
-      return undefined;
-   }
-   else if ( computerSelection === "rock"){
-      switch(playerSelection){
-         case "paper":
-            return true;
-            break;
-         case "scissors":
-            return false;
-            break;
-
-      }
-   }
-   else if ( computerSelection === "paper"){
-            switch(playerSelection){
-         case "rock":
-            return false;
-            break;
-         case "scissors":
-            return true;
-            break;
-            
-      }
-   }
-   else if ( computerSelection === "scissors"){
-      switch(playerSelection){
-         case "paper":
-            return false;
-            break;
-         case "rock":
-            return true;
-            break;
-            
-      }
-   }
+	if (playerSelection === computerSelection) {
+		return undefined;
+	} else if (computerSelection === "rock") {
+		switch (playerSelection) {
+			case "paper":
+				return true;
+				break;
+			case "scissors":
+				return false;
+				break;
+		}
+	} else if (computerSelection === "paper") {
+		switch (playerSelection) {
+			case "rock":
+				return false;
+				break;
+			case "scissors":
+				return true;
+				break;
+		}
+	} else if (computerSelection === "scissors") {
+		switch (playerSelection) {
+			case "paper":
+				return false;
+				break;
+			case "rock":
+				return true;
+				break;
+		}
+	}
 }
 
+let score = 0;
+const results = document.getElementById("results");
+const buttonsContainer = document.getElementById("buttons-container");
+let output_message = document.createElement("p");
 
-   let score = 0;
-   const buttonsContainer = document.getElementById('buttons-container');
-   const button_rock = document.createElement('button');
-   button_rock.textContent = "Rock";
-   button_rock.addEventListener("click", function() {
+const button_rock = document.createElement("button");
+button_rock.textContent = "Rock";
+const button_paper = document.createElement("button");
+button_paper.textContent = "Paper";
+const button_scissors = document.createElement("button");
+button_scissors.textContent = "Scissors";
 
-      const computerSelection = getComputerChoice();
-      playerSelection = "rock";
-      console.log(playerSelection, computerSelection);
+button_rock.addEventListener("click", function handleButtonClick() {
+	const computerSelection = getComputerChoice();
+	playerSelection = "rock";
+	console.log(playerSelection, computerSelection);
 
-      if((playRound(playerSelection, computerSelection)) === true){
-         console.log("Congratulations! You won this round!");
-         score++;
-      }
-      else if (playRound(playerSelection, computerSelection) == undefined) {
-         console.log("It's a tie!");
-      }
-      else{
-         console.log("Unfortunately, you lost this round!")
-      }
-   });
+	if (playRound(playerSelection, computerSelection) === true) {
+		score++;
+		output_message.innerText = `Congratulations! You won this round!\n${score}`;
+		if (score === 5) {
+			output_message.innerText += `\nYou won the game!`;
+			disable_buttons();
+		}
+	} else if (playRound(playerSelection, computerSelection) == undefined) {
+		output_message.innerText = `It's a tie!\n${score}`;
+	} else {
+		score--;
+		output_message.innerText = `Unfortunately, you lost this round! \n${score}`;
+		if (score === -5) {
+			output_message.innerText += `\nYou lost the game loser!`;
+			disable_buttons();
+		}
+	}
+});
 
-   const button_paper = document.createElement('button');
-   button_paper.textContent = "Paper";
-   button_paper.addEventListener("click", function() {
+button_paper.addEventListener("click", function handleButtonClick() {
+	const computerSelection = getComputerChoice();
+	playerSelection = "paper";
+	console.log(playerSelection, computerSelection);
 
-      const computerSelection = getComputerChoice();
-      playerSelection = "paper";
-      console.log(playerSelection, computerSelection);
+	if (playRound(playerSelection, computerSelection) === true) {
+		score++;
+		output_message.innerText = `Congratulations! You won this round!\n${score}`;
+		if (score === 5) {
+			output_message.innerText += `\nYou won the game!`;
+			disable_buttons();
+		}
+	} else if (playRound(playerSelection, computerSelection) == undefined) {
+		output_message.innerText = `It's a tie!\n${score}`;
+	} else {
+		score--;
+		output_message.innerText = `Unfortunately, you lost this round! \n${score}`;
+		if (score === -5) {
+			output_message.innerText += `\nYou lost the game loser!`;
+			disable_buttons();
+		}
+	}
+});
 
-      if((playRound(playerSelection, computerSelection)) === true){
-         console.log("Congratulations! You won this round!");
-         score++;
-      }
-      else if (playRound(playerSelection, computerSelection) == undefined) {
-         console.log("It's a tie!");
-      }
-      else{
-         console.log("Unfortunately, you lost this round!")
-      }
-   });
+button_scissors.addEventListener("click", function handleButtonClick() {
+	const computerSelection = getComputerChoice();
+	playerSelection = "scissors";
+	console.log(playerSelection, computerSelection);
 
-   const button_scissors = document.createElement('button');
-   button_scissors.textContent = "Scissors";
-   button_scissors.addEventListener("click", function() {
+	if (playRound(playerSelection, computerSelection) === true) {
+		score++;
+		output_message.innerText = `Congratulations! You won this round! \n${score}`;
+		if (score === 5) {
+			output_message.innerText += `\nYou won the game!`;
+			disable_buttons();
+		}
+	} else if (playRound(playerSelection, computerSelection) == undefined) {
+		output_message.innerText = `It's a tie!\n${score}`;
+	} else {
+		score--;
+		output_message.innerText = `Unfortunately, you lost this round! \n${score}`;
+		if (score === -5) {
+			output_message.innerText += `\nYou lost the game loser!`;
+			disable_buttons();
+		}
+	}
+});
 
-      const computerSelection = getComputerChoice();
-      playerSelection = "scissors";
-      console.log(playerSelection, computerSelection);
-
-      if((playRound(playerSelection, computerSelection)) === true){
-         console.log("Congratulations! You won this round!");
-         score++;
-      }
-      else if (playRound(playerSelection, computerSelection) == undefined) {
-         console.log("It's a tie!");
-      }
-      else{
-         console.log("Unfortunately, you lost this round!")
-      }
-   });
-
-   buttonsContainer.appendChild(button_rock);
-   buttonsContainer.appendChild(button_scissors);
-   buttonsContainer.appendChild(button_paper);
-
-      //const playerSelection = prompt("Rock/Paper/Scissors");
-      //console.log(playerSelection, computerSelection);
-      /*
-      if((playRound(playerSelection, computerSelection)) === true){
-         console.log("Congratulations! You won this round!");
-         score++;
-      }
-      else if (playRound(playerSelection, computerSelection) == undefined) {
-         console.log("It's a tie!");
-      }
-      else{
-         console.log("Unfortunately, you lost this round!")
-      }
-      */
+buttonsContainer.appendChild(button_rock);
+buttonsContainer.appendChild(button_scissors);
+buttonsContainer.appendChild(button_paper);
+results.appendChild(output_message);
